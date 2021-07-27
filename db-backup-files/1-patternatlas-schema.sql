8a4f1e8c0d86d1b6f644433b353582281f630121
manuwei <44226745+manuwei@users.noreply.github.com>
2021-07-27 18:10:58 +0200

    fix sql file

--- db-backup-files/1-patternatlas-schema.sql
+++ db-backup-files/1-patternatlas-schema.sql
@@ -9,12 +9,6 @@ SET default_transaction_read_only = off;
 SET client_encoding = 'UTF8';
 SET standard_conforming_strings = on;
 
---
--- Roles
---
-
-CREATE ROLE postgres;
-ALTER ROLE postgres WITH SUPERUSER INHERIT CREATEROLE CREATEDB LOGIN REPLICATION BYPASSRLS PASSWORD 'md53175bce1d3201d16594cebf9d7eb3f9d';
 
 
 
